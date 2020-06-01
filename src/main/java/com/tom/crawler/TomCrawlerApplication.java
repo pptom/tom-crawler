c194a47584ec34e5ad0387141ac291f993ad7137
Tom <424222352@qq.com>
2020-06-01 14:06:50 +0800

    更新到spring boot

---
+++ src/main/java/com/tom/crawler/TomCrawlerApplication.java
@@ -0,0 +1,15 @@
+package com.tom.crawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Tom on 2017/3/24.
+ */
+@SpringBootApplication
+public class TomCrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TomCrawlerApplication.class, args);
+    }
+}
