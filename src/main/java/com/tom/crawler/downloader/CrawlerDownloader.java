b0aeff076a9fb0d52ea31fc6df86de73f52be6fa
pptom <424222352@qq.com>
2020-06-03 08:23:37 +0800

    重构downloader

--- src/main/java/com/tom/crawler/downloader/CrawlerDownloader.java
+++ src/main/java/com/tom/crawler/downloader/CrawlerDownloader.java
@@ -1,23 +1,149 @@
 package com.tom.crawler.downloader;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.http.*;
 import org.apache.http.annotation.Contract;
 import org.apache.http.annotation.ThreadingBehavior;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.conn.HttpHostConnectException;
+import org.apache.http.entity.BufferedHttpEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Request;
+import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Task;
 import us.codecraft.webmagic.downloader.AbstractDownloader;
+import us.codecraft.webmagic.downloader.HttpClientRequestContext;
+import us.codecraft.webmagic.downloader.HttpUriRequestConverter;
+import us.codecraft.webmagic.proxy.Proxy;
+import us.codecraft.webmagic.proxy.ProxyProvider;
+import us.codecraft.webmagic.selector.PlainText;
+import us.codecraft.webmagic.utils.CharsetUtils;
+import us.codecraft.webmagic.utils.HttpClientUtils;
+
+import javax.net.ssl.SSLException;
+import javax.net.ssl.SSLHandshakeException;
+import java.io.IOException;
+import java.net.SocketTimeoutException;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
 
 @Contract(threading = ThreadingBehavior.SAFE)
 public class CrawlerDownloader extends AbstractDownloader {
 
+    private final Logger logger = LoggerFactory.getLogger(CrawlerDownloader.class);
+
+    private final TomHttpClientGenerator tomHttpClientGenerator = new TomHttpClientGenerator();
+
+    private final HttpUriRequestConverter httpUriRequestConverter = new HttpUriRequestConverter();
+
+    private final Map<String, CloseableHttpClient> httpClients = new HashMap<>();
+
+    private ProxyProvider proxyProvider;
+
+    private CloseableHttpClient getHttpClient(Site site) {
+        if (site == null) {
+            return tomHttpClientGenerator.getClient(null);
+        }
+        String domain = site.getDomain();
+        CloseableHttpClient httpClient = httpClients.get(domain);
+        synchronized (this) {
+            if (httpClient == null) {
+                httpClient = httpClients.get(domain);
+                if (httpClient == null) {
+                    httpClient = tomHttpClientGenerator.getClient(site);
+                    httpClients.put(domain, httpClient);
+                }
+            }
+        }
+        return httpClient;
+    }
 
     @Override
     public Page download(Request request, Task task) {
-        return null;
+        if (task == null || task.getSite() == null) {
+            throw new NullPointerException("task or site can not be null");
+        }
+        CloseableHttpResponse httpResponse = null;
+        CloseableHttpClient httpClient = getHttpClient(task.getSite());
+        Proxy proxy = proxyProvider != null ? proxyProvider.getProxy(task) : null;
+        HttpClientRequestContext requestContext = httpUriRequestConverter.convert(request, task.getSite(), proxy);
+        Page page = Page.fail();
+        try {
+            httpResponse = httpClient.execute(requestContext.getHttpUriRequest(), requestContext.getHttpClientContext());
+            page = handleResponse(request, request.getCharset() != null ? request.getCharset() : task.getSite().getCharset(), httpResponse, task);
+            onSuccess(request);
+            logger.debug("downloading page success {}", request.getUrl());
+        } catch (IOException e) {
+            if (e instanceof ConnectionClosedException) {
+                logger.error("Premature end of chunk coded message body: {}", request.getUrl());
+            } else if (e instanceof SSLHandshakeException) {
+                logger.error("Remote host closed connection during handshake: {}", request.getUrl());
+            } else if (e instanceof SSLException) {
+                logger.error("SSL peer shut down incorrectly:[HttpClient]  {}", request.getUrl());
+            } else if (e instanceof SocketTimeoutException) {
+                logger.error("download page time out:{}", request.getUrl());
+            } else if (e instanceof NoHttpResponseException) {
+                logger.error("failed to respond:{}", request.getUrl());
+            } else if (e instanceof HttpHostConnectException) {
+                logger.error("Connect to proxy timed out:{}", request.getUrl());
+            } else if (e instanceof TruncatedChunkException) {
+                logger.error("TruncatedChunkException:{}, msg:{}", request.getUrl(), e.getMessage());
+            } else {
+                logger.error("download page error:{} ", request.getUrl(), e);
+            }
+            onError(request);
+        } finally {
+            if (httpResponse != null) {
+                //ensure the connection is released back to pool
+                EntityUtils.consumeQuietly(httpResponse.getEntity());
+            }
+            if (proxyProvider != null && proxy != null) {
+                proxyProvider.returnProxy(proxy, page, task);
+            }
+        }
+        return page;
+    }
+
+    protected Page handleResponse(Request request, String charset, HttpResponse httpResponse, Task task) throws IOException {
+        HttpEntity entity = httpResponse.getEntity();
+        if (entity != null) {
+            entity = new BufferedHttpEntity(entity);
+        }
+        byte[] bytes = IOUtils.toByteArray(entity.getContent());
+        String contentType = entity.getContentType() == null ? "" : entity.getContentType().getValue();
+        Page page = new Page();
+        page.setBytes(bytes);
+        if (!request.isBinaryContent()){
+            if (charset == null) {
+                charset = getHtmlCharset(contentType, bytes);
+            }
+            page.setCharset(charset);
+            page.setRawText(new String(bytes, charset));
+        }
+        page.setUrl(new PlainText(request.getUrl()));
+        page.setRequest(request);
+        page.setStatusCode(httpResponse.getStatusLine().getStatusCode());
+        page.setDownloadSuccess(true);
+        page.setHeaders(HttpClientUtils.convertHeaders(httpResponse.getAllHeaders()));
+        return page;
+    }
+
+    private String getHtmlCharset(String contentType, byte[] contentBytes) throws IOException {
+        String charset = CharsetUtils.detectCharset(contentType, contentBytes);
+        if (charset == null) {
+            charset = Charset.defaultCharset().name();
+            logger.warn("Charset autodetect failed, use {} as charset. Please specify charset in Site.setCharset()", Charset.defaultCharset());
+        }
+        return charset;
     }
 
     @Override
     public void setThread(int threadNum) {
-
+        tomHttpClientGenerator.setPoolSize(threadNum);
     }
 }
---
+++ src/main/java/com/tom/crawler/downloader/TomHttpClientGenerator.java
@@ -0,0 +1,186 @@
+package com.tom.crawler.downloader;
+
+import org.apache.http.HttpException;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpRequestInterceptor;
+import org.apache.http.client.CookieStore;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.config.SocketConfig;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.*;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.impl.cookie.BasicClientCookie;
+import org.apache.http.protocol.HttpContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.downloader.CustomRedirectStrategy;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.Map;
+
+/**
+ * @author tom.tang
+ * @date 2018/7/6
+ * @email tom.tang@sainstore.com
+ * @description
+ * @since 2018/7/6
+ */
+public class TomHttpClientGenerator {
+    private final Logger logger = LoggerFactory.getLogger(TomHttpClientGenerator.class);
+
+    private PoolingHttpClientConnectionManager connectionManager;
+
+    public TomHttpClientGenerator() {
+        Registry<ConnectionSocketFactory> reg = RegistryBuilder.<ConnectionSocketFactory>create()
+                .register("http", PlainConnectionSocketFactory.INSTANCE)
+                .register("https", buildSSLConnectionSocketFactory())
+                .build();
+        connectionManager = new PoolingHttpClientConnectionManager(reg);
+        connectionManager.setDefaultMaxPerRoute(100);
+    }
+
+    private SSLConnectionSocketFactory buildSSLConnectionSocketFactory() {
+        // 实现一个X509TrustManager接口，用于绕过验证，不用修改里面的方法
+        X509TrustManager trustManager = new X509TrustManager() {
+
+            @Override
+            public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+            }
+
+            @Override
+            public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+            }
+
+            @Override
+            public X509Certificate[] getAcceptedIssuers() {
+                return null;
+            }
+
+        };
+        X509TrustManager[] xtmArray = new X509TrustManager[] {
+                trustManager
+        };
+
+        SSLContext sslContext = null;
+        try {
+            sslContext = SSLContext.getInstance("TLS");
+
+        } catch (NoSuchAlgorithmException e) {
+            logger.warn("get sslContext instance error! {}", e.getMessage());
+        }
+        try {
+            sslContext.init(null, xtmArray, new SecureRandom());
+        } catch (KeyManagementException e) {
+            logger.warn("init sslcontext error! {}", e.getMessage());
+        }
+        String[] supportedProtocols = new String[]{"TLSv1", "TLSv1.1", "TLSv1.2"};
+        SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(
+                sslContext, supportedProtocols, null,
+                //客户端验证服务器身份的策略
+                SSLConnectionSocketFactory.getDefaultHostnameVerifier());
+
+        return sslsf;
+    }
+
+    private SSLContext createIgnoreVerifySSL() throws NoSuchAlgorithmException, KeyManagementException {
+        // 实现一个X509TrustManager接口，用于绕过验证，不用修改里面的方法
+        X509TrustManager trustManager = new X509TrustManager() {
+
+            @Override
+            public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+            }
+
+            @Override
+            public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+            }
+
+            @Override
+            public X509Certificate[] getAcceptedIssuers() {
+                return null;
+            }
+
+        };
+
+        SSLContext sc = SSLContext.getInstance("SSLv3");
+        sc.init(null, new TrustManager[] { trustManager }, null);
+        return sc;
+    }
+
+    public TomHttpClientGenerator setPoolSize(int poolSize) {
+        connectionManager.setMaxTotal(poolSize);
+        return this;
+    }
+
+    public CloseableHttpClient getClient(Site site) {
+        return generateClient(site);
+    }
+
+    private CloseableHttpClient generateClient(Site site) {
+        HttpClientBuilder httpClientBuilder = HttpClients.custom();
+
+        httpClientBuilder.setConnectionManager(connectionManager);
+        if (site.getUserAgent() != null) {
+            httpClientBuilder.setUserAgent(site.getUserAgent());
+        } else {
+            httpClientBuilder.setUserAgent("");
+        }
+        if (site.isUseGzip()) {
+            httpClientBuilder.addInterceptorFirst(new HttpRequestInterceptor() {
+
+                public void process(
+                        final HttpRequest request,
+                        final HttpContext context) throws HttpException, IOException {
+                    if (!request.containsHeader("Accept-Encoding")) {
+                        request.addHeader("Accept-Encoding", "gzip");
+                    }
+                }
+            });
+        }
+        //解决post/redirect/post 302跳转问题
+        httpClientBuilder.setRedirectStrategy(new CustomRedirectStrategy());
+
+        SocketConfig.Builder socketConfigBuilder = SocketConfig.custom();
+        socketConfigBuilder.setSoKeepAlive(true).setTcpNoDelay(true);
+        socketConfigBuilder.setSoTimeout(site.getTimeOut());
+        SocketConfig socketConfig = socketConfigBuilder.build();
+        httpClientBuilder.setDefaultSocketConfig(socketConfig);
+        connectionManager.setDefaultSocketConfig(socketConfig);
+        httpClientBuilder.setRetryHandler(new DefaultHttpRequestRetryHandler(site.getRetryTimes(), true));
+        generateCookie(httpClientBuilder, site);
+        return httpClientBuilder.build();
+    }
+
+    private void generateCookie(HttpClientBuilder httpClientBuilder, Site site) {
+        if (site.isDisableCookieManagement()) {
+            httpClientBuilder.disableCookieManagement();
+            return;
+        }
+        CookieStore cookieStore = new BasicCookieStore();
+        for (Map.Entry<String, String> cookieEntry : site.getCookies().entrySet()) {
+            BasicClientCookie cookie = new BasicClientCookie(cookieEntry.getKey(), cookieEntry.getValue());
+            cookie.setDomain(site.getDomain());
+            cookieStore.addCookie(cookie);
+        }
+        for (Map.Entry<String, Map<String, String>> domainEntry : site.getAllCookies().entrySet()) {
+            for (Map.Entry<String, String> cookieEntry : domainEntry.getValue().entrySet()) {
+                BasicClientCookie cookie = new BasicClientCookie(cookieEntry.getKey(), cookieEntry.getValue());
+                cookie.setDomain(domainEntry.getKey());
+                cookieStore.addCookie(cookie);
+            }
+        }
+        httpClientBuilder.setDefaultCookieStore(cookieStore);
+    }
+
+}
